b6cd5590008ceaf5a8a3280bcd1350d95cf6a7dd
EddieTheCubeHead <eetu.asikainen1204@gmail.com>
2022-05-24 15:03:52 +0300

    Correct python version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(name="HinteDI",
       classifiers=[
           "Development Status :: 4 - Beta",
           "Intended Audience :: Developers",
-          "Programming language :: Python :: 3",
+          "Programming language :: Python :: 3.10",
           "License :: OSI Approved :: MIT License",
           "Operating System :: OS Independent"
       ],
